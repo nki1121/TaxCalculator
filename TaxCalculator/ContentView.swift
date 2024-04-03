792f6c1ffcc9c96f1c3392d47875d48ae32e9d20
nki1121 <nkiuniv@gmail.com>
2024-04-03 23:45:37 +0900

    Initial Commit

---
+++ TaxCalculator/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TaxCalculator
+//
+//  Created by 中井晋平 on 2024/04/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TaxCalculator/TaxCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxCalculatorApp.swift
+//  TaxCalculator
+//
+//  Created by 中井晋平 on 2024/04/03.
+//
+
+import SwiftUI
+
+@main
+struct TaxCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

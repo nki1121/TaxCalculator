565d04b3b2e5e00f519ded73685ef12e8721f761
nki1121 <nkiuniv@gmail.com>
2024-04-06 23:53:46 +0900

    計算結果をTextに表示

--- TaxCalculator/ContentView.swift
+++ TaxCalculator/ContentView.swift
@@ -16,11 +16,12 @@ struct ContentView: View {
         VStack(spacing: 20) {
             TextField("ここに文字を入力", text: $inputText)
             Button("計算") {
-                
+                tax8 = Double(inputText)! * 0.08
+                tax10 = Double(inputText)! * 0.1
             }
             Text("価格：\(inputText)")
-            Text("消費税8%：")
-            Text("消費税10%：")
+            Text("消費税8%：\(tax8)")
+            Text("消費税10%：\(tax10)")
         }
         .padding()
     }
